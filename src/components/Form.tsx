a435077275014498a9d7204b1423e204726939ea
roblkdeboer <roblkdeboer@gmail.com>
2023-03-06 22:05:34 +0800

    Tidy up form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,13 +1,25 @@
 import React, { useState } from 'react';
-import { TextField, Typography, Button, Grid } from '@material-ui/core';
+import {
+  Card,
+  CardContent,
+  CardActions,
+  TextField,
+  Typography,
+  Button,
+} from '@material-ui/core';
+import { styled } from '@mui/material/styles';
+
+const CustomCard = styled(Card)({
+  margin: '2rem',
+}) as typeof Card;
 
 export default function Form() {
   const [inputValue, setInputValue] = useState('');
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(`Submitted value: ${inputValue}`);
-    setInputValue('');
+    const url = `https://wa.me/${inputValue}`;
+    window.open(url, '_blank');
   };
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -15,25 +27,38 @@ export default function Form() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <Grid container direction="column" spacing={2}>
-        <Grid item>
-          <Typography variant="body1">Please enter your name:</Typography>
-        </Grid>
-        <Grid item>
-          <TextField
-            label="Name"
-            variant="outlined"
-            value={inputValue}
-            onChange={handleInputChange}
-          />
-        </Grid>
-        <Grid item>
-          <Button type="submit" variant="contained" color="primary">
-            Submit
-          </Button>
-        </Grid>
-      </Grid>
-    </form>
+    <div>
+      <CustomCard>
+        <form onSubmit={handleSubmit}>
+          <CardContent>
+            <Typography variant="h4" gutterBottom>
+              WhatsApp Anyone
+            </Typography>
+            <Typography variant="body1">
+              Are you sick of having to add people to contacts before you
+              message them on WhatsApp? Enter the phone number of the person you
+              wish to contact in the following format: area code + phone number:
+              6591234567
+            </Typography>
+          </CardContent>
+          <CardContent>
+            <TextField
+              id="outlined-helperText"
+              label="Number"
+              variant="outlined"
+              value={inputValue}
+              onChange={handleInputChange}
+              helperText="Do not include the +"
+            />
+          </CardContent>
+
+          <CardContent>
+            <Button type="submit" variant="contained" color="secondary">
+              Submit
+            </Button>
+          </CardContent>
+        </form>
+      </CustomCard>
+    </div>
   );
 }
