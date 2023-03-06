9c95ede7739a613c1ad58e26092a44ab053f1c45
roblkdeboer <roblkdeboer@gmail.com>
2023-03-06 19:37:18 +0800

    Tidy up header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,21 @@
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Box, Toolbar, Typography } from '@material-ui/core';
 import Logo from '../assets/logo.png';
 import React from 'react';
 
 export default function Header() {
-  const displayDesktop = () => {
-    return (
-      <Toolbar>
-        <img
-          src={Logo}
-          alt="My App Icon"
-          style={{ width: '2rem', marginRight: '1rem' }}
-        />{' '}
-        {/* App icon */}
-        <Typography variant="h5">Whatsapp Anyone</Typography>
-      </Toolbar>
-    );
-  };
-
   return (
-    <header>
-      <AppBar>{displayDesktop()}</AppBar>
-    </header>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <img
+            src={Logo}
+            alt="My App Icon"
+            style={{ width: '2rem', marginRight: '1rem' }}
+          />{' '}
+          {/* App icon */}
+          <Typography variant="h5">Whatsapp Anyone</Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
