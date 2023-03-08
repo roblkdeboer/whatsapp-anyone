b0f8ecf7fa0ad2584c6d6fd8b5dd8d6f5ca14b60
roblkdeboer <roblkdeboer@gmail.com>
2023-03-08 15:06:28 +0800

    Turn off auto complete for input

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -34,21 +34,25 @@ export default function Form() {
             <Typography variant="h4" gutterBottom>
               WhatsApp Anyone
             </Typography>
-            <Typography variant="body1">
+            <Typography variant="body1" gutterBottom>
               Are you sick of having to add people to contacts before you
-              message them on WhatsApp? Enter the phone number of the person you
-              wish to contact in the following format: area code + phone number:
-              6591234567
+              message them on WhatsApp?
+            </Typography>
+            <Typography variant="body1">
+              Enter the phone number of the person you wish to contact in the
+              following format: area code + phone number: 6591234567
             </Typography>
           </CardContent>
           <CardContent>
             <TextField
               id="outlined-helperText"
+              placeholder="6591234567"
               label="Number"
               variant="outlined"
               value={inputValue}
               onChange={handleInputChange}
               helperText="Do not include the +"
+              autoComplete="off"
             />
           </CardContent>
 
